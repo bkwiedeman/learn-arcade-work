d45d6edc569105dd672f09d4a7b4da27879d1a18
bkwiedeman <braydenwiedeman@mail.weber.edu>
2022-01-19 18:34:10 -0700

    changes to lab_01.py

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,6 @@
+print("My name is Brayden Wiedeman.")
+print("I am taking this course as a requirement for my Minor in Computer Science.")
+print("The reason I deceided to make Computer Science my minor because the way the world is going now, Computers are the way of the future.")
+print("While I do not like that computers are the way of the future. When it comes to technology, I feel like Mama in The Waterboy, Technology \"Is the Devil\"")
+print("But I can not change the fact that technology is the way of the future.. so as the saying goes \"If you cant "
+      "beat \'em, Join \'em\"")
